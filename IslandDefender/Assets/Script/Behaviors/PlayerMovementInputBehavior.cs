796284dbe851ac8a7c83e882ce1e1521e008afdc
Fravic <fravicf@gmail.com>
2014-11-20 23:12:26 -0800

    Refactor PlayerMovementInputBehavior

--- IslandDefender/Assets/Script/Behaviors/PlayerMovementInputBehavior.cs
+++ IslandDefender/Assets/Script/Behaviors/PlayerMovementInputBehavior.cs
@@ -5,18 +5,18 @@ using System.Collections.Generic;
 
 public class PlayerMovementInputBehavior : MonoBehaviour {
   public bool inputEnabled;
-
   private ShipEntity _entity;
 
-  private float _lastAngle;
-  private Queue<float> _angleDiffs;
-  private Vector2 _lastRegTouchPos;
-
-  private const int _minTouchDelta = 5;
+  private TouchDistValidator _touchDistV;
+  private AngleDiffTransformer _angleDiffT;
+  private AngleQueueTransformer _angleQueueT;
 
   public void Awake() {
     _entity = gameObject.GetComponent<ShipEntity>();
-    _angleDiffs = new Queue<float>();
+
+    _touchDistV = new TouchDistValidator();
+    _angleDiffT = new AngleDiffTransformer();
+    _angleQueueT = new AngleQueueTransformer();
   }
 
   public void Update() {
@@ -26,43 +26,10 @@ public class PlayerMovementInputBehavior : MonoBehaviour {
     
     if (Input.touchCount > 0) {
       Touch touch = Input.GetTouch(0);
-
       if (touch.phase == TouchPhase.Began) {
-	_lastRegTouchPos = touch.position;
-	_lastAngle = 1000; // 'Unset' flag: refactor this
-	_angleDiffs.Clear();
-
+	TouchBegan(touch);
       } else if (touch.phase == TouchPhase.Moved) {
-	Vector2 touchDelta = touch.position - _lastRegTouchPos;
-	if (touchDelta.magnitude > _minTouchDelta) {
-	  _lastRegTouchPos = touch.position;
-	  float angle = (float)Math.Atan2(touch.deltaPosition.y, touch.deltaPosition.x) * 180 / (float)Math.PI;
-	  angle = Utils.BoundAngle(angle);
-
-	  if (angle != _lastAngle && _lastAngle != 1000) {
-	    if (angle > 270 && _lastAngle < 90) {
-	      _lastAngle += 360;
-	    } else if (_lastAngle > 270 && angle < 90) {
-	      angle += 360;
-	    }
-	    _angleDiffs.Enqueue(angle - _lastAngle);
-	  }
-	  _lastAngle = Utils.BoundAngle(angle);
-	}
-
-	if (_angleDiffs.Count > 5) {
-	  float sum = Utils.SumArray(_angleDiffs.ToArray(), 0.0f);
-	  if (sum > 20) {
-	    _entity.targetOrientation += 20;
-	  } else if (sum < -20) {
-	    _entity.targetOrientation -= 20;
-	  }
-	  _angleDiffs.Clear();
-	}
-
-      } else if (touch.phase == TouchPhase.Ended) {
-	_entity.targetOrientation = _entity.orientation;
-
+	TouchMoved(touch);
       }
 
       _entity.UpdateOrientation();
@@ -70,4 +37,116 @@ public class PlayerMovementInputBehavior : MonoBehaviour {
     }
   }
 
+  private void TouchBegan(Touch touch) {
+    _touchDistV.Reset();
+    _angleDiffT.Reset();
+    _angleQueueT.Reset();
+  }
+
+  private void TouchMoved(Touch touch) {
+    if (_touchDistV.IsValid(touch)) {
+      float angleDiff = _angleDiffT.Transform(touch);
+      float force = _angleQueueT.Transform(angleDiff);
+      _entity.targetOrientation += force;
+    }
+  }
+
+
+  /* TouchDistTransformer filters raw touches for significant distance */
+  private class TouchDistValidator {
+    private const int MIN_TOUCH_DELTA = 5;
+
+    private Vector2 _lastRegTouchPos = Vector2.zero;
+    private bool _lastRegTouchPosSet = false;
+
+    public bool IsValid(Touch input) {
+      if (_lastRegTouchPosSet) {
+	Vector2 touchDelta = input.position - _lastRegTouchPos;
+	if (touchDelta.magnitude > MIN_TOUCH_DELTA) {
+	  SetLastRegTouchPos(input.position);
+	  return true;
+	}
+      }
+      SetLastRegTouchPos(input.position);
+      return false;
+    }
+
+    public void Reset() {
+      _lastRegTouchPosSet = false;
+    }
+
+    private void SetLastRegTouchPos(Vector2 val) {
+      _lastRegTouchPos = val;
+      _lastRegTouchPosSet = true;
+    }
+  }
+
+
+  /* AngleDiffTransformer keeps track of touch angles and returns their
+     differences */
+  private class AngleDiffTransformer {
+    private float _lastAngle = 0;
+    private bool _lastAngleSet = false;
+
+    public float Transform(Touch input) {
+      float diff = 0;
+      float angle = (float)Math.Atan2(input.deltaPosition.y,
+				      input.deltaPosition.x)
+	                     * 180 / (float)Math.PI;
+      angle = Utils.BoundAngle(angle);
+
+      if (angle != _lastAngle && _lastAngleSet) {
+	if (angle > 270 && _lastAngle < 90) {
+	  _lastAngle += 360;
+	} else if (_lastAngle > 270 && angle < 90) {
+	  angle += 360;
+	}
+	diff = angle - _lastAngle;
+      }
+
+      SetLastAngle(angle);
+      return diff;
+    }
+
+    public void Reset() {
+      _lastAngleSet = false;
+    }
+
+    private void SetLastAngle(float val) {
+      _lastAngle = Utils.BoundAngle(val);
+      _lastAngleSet = true;
+    }
+  }
+
+
+  /* AngleQueueTransformer keeps a queue of angle differences until it gets
+     big enough, then returns a force vector to apply to the entity. */
+  private class AngleQueueTransformer {
+    private const int ANGLE_DIFFS_NEEDED = 5;
+    private const float FORCE_MULTIPLIER = 20;
+    private const float MIN_NECESSARY_SUM = 10;
+
+    private Queue<float> _angleDiffs = new Queue<float>();
+
+    public float Transform(float input) {
+      if (input == 0) return 0;
+
+      _angleDiffs.Enqueue(input);
+      if (_angleDiffs.Count > ANGLE_DIFFS_NEEDED) {
+	float sum = Utils.SumArray(_angleDiffs.ToArray(), 0.0f);
+	_angleDiffs.Clear();
+
+	if (sum > MIN_NECESSARY_SUM) {
+	  return FORCE_MULTIPLIER;
+	} else if (sum < -MIN_NECESSARY_SUM) {
+	  return -FORCE_MULTIPLIER;
+	}
+      }
+      return 0;
+    }
+
+    public void Reset() {
+      _angleDiffs.Clear();
+    }
+  }
 }
